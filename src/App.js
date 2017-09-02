133a4e45565ddcf99b7420b43f933ee76a665318
Tomas Kosegi <tomas.kosegi@gmail.com>
2017-09-02 02:28:52 +0200

    first measure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
 import React, { PureComponent } from 'react';
 // import { connect } from 'react-redux';
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 // import actionCreators from './redux/actionCreators';
 import Page from './styled/page';
+
+// const range = (amount) => {
+//   let range = [];
+//   for (let i = 1; i < amount+1; i++ ) {
+//     range.push(`target-${i}`);
+//   }
+//   return range;
+// }
+//
+// class getCoords {
+//   constructor() {
+//
+//   }
+//
+//
+// }
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
+      showModal: false,
+      coordinates: {
+        x: null,
+        y: null,
+        top: null,
+        bottom: null,
+        right: null,
+        left: null,
+      },
+      targets: Array.from({length: 8}, (_,i) => `target-${i+1}`),
+      active: null,
     }
   }
 
   componentDidMount() {
+
   }
 
   componentWillUnmount() {
   }
 
-  handleClick = () => {
+  handleClickOnTarget = (ref, target) => {
+    const coords = ref.getBoundingClientRect();
+    this.setState({
+      coordinates: {
+        x: Math.round(coords.x),
+        y: Math.round(coords.y),
+        top: Math.round(coords.bottom) || null,
+        left: Math.round(coords.left) || null,
+        bottom: Math.round(coords.bottom) || null,
+        right: Math.round(coords.right) || null,
+      },
+      active: target,
+      showModal: true,
+    })
+  }
+
+  handleCloseModal = () => {
+    this.setState({showModal: false, active: null})
   }
 
   render() {
     return (
       <Page
+        {...this.state}
+        handleClick={this.handleClickOnTarget}
       />
     );
   }
--- src/styled/page.jsx
+++ src/styled/page.jsx
@@ -21,15 +21,6 @@ const Container = styled.div`
   }
 `;
 
-const ScrollIndicator = styled.div.attrs({
-  scale: props => (props.progress / 100 ) || 0,
-})`
-  background-color: var(--color);
-  height: 1em;
-  transform: scaleX(${props => props.scale});
-  transform-origin: left;
-`;
-
 const ScrollIndicatorDescription = styled.h2`
   font-size: 2em;
   color: var(--color);
@@ -37,16 +28,92 @@ const ScrollIndicatorDescription = styled.h2`
   margin-bottom: calc(var(--vertical-rhytm)/2);
 `;
 
+const TargetsContainer = styled.div`
+  width: 100%;
+  height: 10em;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  --color-active: ${props => props.active ? 'orange' : 'black'};
+`
+
+const TestRef = ({className, children, handleClick, number, target}) => {
+  let ref = null;
+  return (
+    <div
+      ref={(elem) => {ref = elem;}}
+      onClick={() => handleClick(ref, target)}
+      className={className}>
+      {children}
+    </div>
+  );
+}
+
+const StyledTestRef = styled(TestRef)`
+  height: 4em;
+  width: 4em;
+  ${'' /* background-color: ${props => props.active ? 'orange' : 'black'}; */}
+  background-color: black;
+  margin: 0.5em 0;
+  position: relative;
+  border-radius: 50%;
+  z-index: 1;
+  align-self: ${props => props.even ? 'flex-end' : 'auto'};
+  ${'' /* transition: background-color 0.2s linear; */}
+  &::before {
+    position: absolute;
+    content: '';
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    ${'' /* height: 66.66%;
+    width: 66.66%; */}
+    height: ${props => props.active ? '102%' : '66.66%'};
+    width: ${props => props.active ? '102%' : '66.66%'};
+    background-color: orange;
+    z-index: 500;
+    border-radius: 50%;
+    transition: all 0.1s linear 0.2s;
+  }
+  &::after {
+    position: absolute;
+    ${props => `content: '${props.number || 0}';`};
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: ${props => props.active ? 'black' : 'white'};
+    height: 33.33%;
+    width: 33.33%;
+    background-color: ${props => props.active ? 'orange' : 'black'};
+    z-index: 1000;
+    border-radius: 50%;
+    transition: background-color 0.2s linear,
+    color 0.2s linear;
+  }
+`;
+
 const Page = ({
   showModal,
   coordinates,
   targets,
-  active
+  active,
+  handleClick,
 }) => (
   <Wrapper>
     <Container>
       <ScrollIndicatorDescription>Scroll progress: </ScrollIndicatorDescription>
-      <ScrollIndicator/>
+      <TargetsContainer>
+        { targets.map((target,i) => (
+          <StyledTestRef
+            key={target}
+            handleClick={handleClick}
+            number={i+1}
+            target={target}
+            even={i%2}
+            active={active === target}/>
+        ))}
+      </TargetsContainer>
     </Container>
   </Wrapper>
 );
