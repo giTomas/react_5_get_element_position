1ce4f5436557e1eed35c49287a9bdd9062184a6b
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2017-09-05 16:47:25 +0200

    cleaning

--- src/styled/page.jsx
+++ src/styled/page.jsx
@@ -1,5 +1,8 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import React from 'react';
+
+import TargetElement from './targetElement';
+import Modal from './modal';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -20,106 +23,6 @@ const GridContainer = styled.div`
   grid-auto-rows: 5em;
 `;
 
-// const Instructions = styled.h2`
-//   font-size: 2em;
-//   color: var(--color);
-//   font-weight: 200;
-//   margin-bottom: calc(var(--vertical-rhytm)/2);
-// `;
-
-
-const showModal = keyframes`
-  from {
-    opacity: 0;
-  }
-`
-// const ModalOverlay = styled.div`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   background-color: rgba(255, 255, 255, 0);
-//
-//   z-index: 400;
-// `;
-
-const ModalContent = styled.div`
-  font-size: 2em;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: 350px;
-  padding: var(--vertical-rhytm) calc(var(--vertical-rhytm)*2);
-  line-height: var(--line-height);
-  background-color: rgba(255, 255, 255, 0.9);
-  box-shadow: 0 0 15px rgba(0, 0, 0, 0.35);
-  animation: 0.5s ${showModal} ease-out;
-  z-index: 400;
-`;
-
-const Modal = ({coordinates, handleCloseModal}) => (
-    <ModalContent
-      onClick={handleCloseModal}>
-        <p>x: {coordinates.x}px</p>
-        <p>y: {coordinates.y}px</p>
-    </ModalContent>
-);
-
-const AttachedRef = ({className, children, handleClick, number, target}) => {
-  let ref = null;
-  return (
-    <div
-      ref={(elem) => {ref = elem;}}
-      onClick={() => handleClick(ref, target)}
-      className={className}>
-      {children}
-    </div>
-  );
-}
-
-const TargetElement = styled(AttachedRef)`
-  height: 4em;
-  width: 4em;
-  --color-active: ${props => props.active ? 'orange' : 'black'};
-  background-color: var(--color-active);
-  margin: 0.5em;
-  position: relative;
-  border-radius: 50%;
-  z-index: 1;
-  align-self: ${props => props.even ? 'flex-end' : 'auto'};
-  transition: background-color 0.2s linear;
-  &::before {
-    position: absolute;
-    content: '';
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    height: 66.66%;
-    width: 66.66%;
-    background-color: orange;
-    z-index: 50;
-    border-radius: 50%;
-  }
-  &::after {
-    position: absolute;
-    ${props => `content: '${props.number || 0}';`};
-    text-align: center;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    color: ${props => props.active ? 'black' : 'white'};
-    height: 33.33%;
-    width: 33.33%;
-    ${'' /* line-height: 2; */}
-    background-color: var(--color-active);
-    z-index: 100;
-    border-radius: 50%;
-    transition: background-color 0.1s linear,
-    color 0.1s linear;
-  }
-`;
 
 const Page = ({
   showModal,
---
+++ src/styled/targetElement.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AttachedRef = ({className, children, handleClick, number, target}) => {
+  let ref = null;
+  return (
+    <div
+      ref={(elem) => {ref = elem;}}
+      onClick={() => handleClick(ref, target)}
+      className={className}>
+      {children}
+    </div>
+  );
+}
+
+const TargetElement = styled(AttachedRef)`
+  height: 4em;
+  width: 4em;
+  --color-active: ${props => props.active ? 'orange' : 'black'};
+  background-color: var(--color-active);
+  margin: 0.5em;
+  position: relative;
+  border-radius: 50%;
+  z-index: 1;
+  align-self: ${props => props.even ? 'flex-end' : 'auto'};
+  transition: background-color 0.2s linear;
+  &::before {
+    position: absolute;
+    content: '';
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 66.66%;
+    width: 66.66%;
+    background-color: orange;
+    z-index: 50;
+    border-radius: 50%;
+  }
+  &::after {
+    position: absolute;
+    ${props => `content: '${props.number || 0}';`};
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: ${props => props.active ? 'black' : 'white'};
+    height: 33.33%;
+    width: 33.33%;
+    ${'' /* line-height: 2; */}
+    background-color: var(--color-active);
+    z-index: 100;
+    border-radius: 50%;
+    transition: background-color 0.1s linear,
+    color 0.1s linear;
+  }
+`;
+
+export default TargetElement;
